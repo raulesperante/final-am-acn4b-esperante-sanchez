7db87688df7a1553f171a90f513a977b46abe30e
Elizabeth Sanchez Teran <115377503+ElizabethST18@users.noreply.github.com>
2024-05-07 13:49:56 -0300

    Vista de Menu Items
    Agregue metodo para crear TextView de Direccion y agregue metodo para crear TextView de Flecha que van junto a RelativeLayout

--- app/src/main/java/ar/edu/davinci/goldengym/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/goldengym/MainActivity.java
@@ -1,17 +1,12 @@
 package ar.edu.davinci.goldengym;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Arrays;
-
-
 import android.os.Bundle;
-import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.EditText;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -48,15 +43,80 @@ public class MainActivity extends AppCompatActivity {
                 "Lavalle 500, Microcentro CABA",
                 "Av. Caseros 3000, CABA",
                 "Av. Belgrano 1120, Avellaneda",
-                "Av. Mitre 560, Avellaneda"
+                "Av. Mitre 560, Avellaneda",
+                "Av. Acoyte 480, Caballito, CABA",
+                "Av. Cordoba 4887, Palermo",
+                "Av. Medrano 148,CABA",
+                "Moreno 369, CABA",
+                "Av. Juramento 223, CABA"
         };
-        Integer[] indices = {0, 1, 2, 3, 4};
-        List<Integer> listaIndices = Arrays.asList(indices);
-        Collections.shuffle(listaIndices);
-        for (Integer indice : listaIndices) {
-            TextView direccion = new TextView(this);
-            direccion.setText(gyms[indice]);
-            resultados.addView(direccion);
+
+        for (String direccion : gyms) {
+            // RelativeLayout
+            RelativeLayout relativeLayout = new RelativeLayout(this);
+
+            // RelativeLayout Parametros
+            RelativeLayout.LayoutParams layoutParametros = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.MATCH_PARENT,
+                    150
+            );
+            layoutParametros.setMargins(0, 0,0, 50);
+            relativeLayout.setLayoutParams(layoutParametros);
+            relativeLayout.setBackgroundResource(R.color.menu_item); // color de fondo
+
+            // Agrego Direccion Gimnasio y Icono Flecha a RelativeLayout
+            relativeLayout.addView(crearTextViewDireccion(direccion));
+            relativeLayout.addView(crearTextViewFlecha());
+
+            // Agrego RelativeLayout a la vista
+            resultados.addView(relativeLayout);
         }
+    }
+
+    private TextView crearTextViewDireccion(String direccion) {
+        int colorTexto = getResources().getColor(R.color.texto_primario);
+
+        // TextView Direccion Gimnasio
+        TextView textViewDireccion = new TextView(this);
+        textViewDireccion.setId(View.generateViewId()); // ID
+        textViewDireccion.setText(direccion);
+        textViewDireccion.setTextColor(colorTexto); // color de texto
+        textViewDireccion.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16); // tamaño del texto
+
+        // TextView Direccion Gimnasio - Parametros
+        RelativeLayout.LayoutParams textViewDireccionParametros = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        textViewDireccionParametros.addRule(RelativeLayout.ALIGN_PARENT_START);
+        textViewDireccionParametros.addRule(RelativeLayout.CENTER_VERTICAL);
+        textViewDireccionParametros.setMarginStart(32); // margen de inicio
+        textViewDireccionParametros.setMarginEnd(8); // margen final
+        textViewDireccion.setLayoutParams(textViewDireccionParametros);
+
+        return textViewDireccion;
+    }
+
+    private TextView crearTextViewFlecha() {
+        int colorTexto = getResources().getColor(R.color.texto_primario);
+
+        // TextView Icono Flecha
+        TextView textViewFlecha = new TextView(this);
+        textViewFlecha.setId(View.generateViewId()); // ID
+        textViewFlecha.setText(R.string.flecha);
+        textViewFlecha.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20); // tamaño del texto
+        textViewFlecha.setTextColor(colorTexto); // color del texto
+
+        // TextView Icono Flecha - Parametros
+        RelativeLayout.LayoutParams textViewFlechaParametros = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        textViewFlechaParametros.addRule(RelativeLayout.ALIGN_PARENT_END);
+        textViewFlechaParametros.addRule(RelativeLayout.CENTER_VERTICAL);
+        textViewFlechaParametros.setMarginEnd(50); // margen derecho
+        textViewFlecha.setLayoutParams(textViewFlechaParametros);
+
+        return textViewFlecha;
     }
 }
