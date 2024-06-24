abf02d186ff4ddf3712349d7f2e0d0f4dfe0db3d
Raúl Esperante <ragusmat@hotmail.com>
2024-06-23 23:12:40 -0300

    LoginActivity primera pantalla. Conecta con pantalla Start

---
+++ app/src/main/java/ar/edu/davinci/goldengym/LoginActivity.java
@@ -0,0 +1,34 @@
+package ar.edu.davinci.goldengym;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class LoginActivity extends  AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+     public void login(View view){
+         EditText mail = findViewById(R.id.email);
+         EditText password = findViewById(R.id.password);
+         if(mail.length() == 0 || password.length() == 0){
+             Toast.makeText(LoginActivity.this,
+                     "Debe completar todos los campos",
+                     Toast.LENGTH_SHORT).show();
+         }else{
+             Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+             startActivity(intent);
+         }
+    }
+}
+
