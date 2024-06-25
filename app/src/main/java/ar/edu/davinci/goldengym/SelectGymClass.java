2c6ca5244b92e7c11417faa45f4ed67b63cd0e0d
Raúl Esperante <ragusmat@hotmail.com>
2024-06-24 21:17:44 -0300

    Conexión pantalla start con seleccionar actividades

--- app/src/main/java/ar/edu/davinci/goldengym/SelectGymClass.java
+++ app/src/main/java/ar/edu/davinci/goldengym/SelectGymClass.java
@@ -1,23 +1,25 @@
 package ar.edu.davinci.goldengym;
 
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class StartActivity extends AppCompatActivity {
+public class SelectGymClass extends  AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start);
+        setContentView(R.layout.select_gym_class);
     }
 
-    public void letsgo(View view){
-        Intent intent = new Intent(getApplicationContext(), SelectGymClass.class);
+    public void foo(View view){
+        Intent intent = new Intent(getApplicationContext(), RegistroActivity.class);
         startActivity(intent);
     }
 }
+
