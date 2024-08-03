f26d2f47d7a4abd5457e46985b99ea1d08b072d4
Raúl Esperante <ragusmat@hotmail.com>
2024-08-02 22:09:11 -0300

    Limpiar código

--- app/src/main/java/ar/edu/davinci/goldengym/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/goldengym/LoginActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Handler;
-import android.util.Log;
 import android.widget.Toast;
 import android.view.View;
 import android.widget.EditText;
@@ -51,25 +49,19 @@ public class LoginActivity extends  AppCompatActivity {
     }
 
     private void signIn(String email, String password) {
-        // [START sign_in_with_email]
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("debug1", "signInWithEmail:success");
                             goToStartActivity();
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("debug1", "signInWithEmail:failure", task.getException());
                             Toast.makeText(LoginActivity.this,
                                     "Email o contraseña incorrecta",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-        // [END sign_in_with_email]
     }
 }
 
